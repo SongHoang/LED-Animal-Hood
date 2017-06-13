2c456c92cbb83e90e9e858cb3ad25552e4fb302e
Song Hoang <isong@uga.edu>
2017-06-13 19:16:12 -0400

    Add quickFlash(), LED's will now flash in and out, and to the beat of music when variables are adjusted

--- ledanimalhood.cpp
+++ ledanimalhood.cpp
@@ -6,7 +6,7 @@ Version 0.1
 
 Author: Song Hoang
 
-Uses Arduino Uno, TLC5947, and Accelerometer
+Uses Arduino Uno, TLC5947 12-bit PWM, and LSM303C Accelerometer
 
 */
 
@@ -24,34 +24,123 @@ unsigned long previousMillis = 0;
 
 int LEDMode = 0;
 
+int LEDValue[8][3];
+
+int doNotWrite = 0;
+
+double bpm = 0;
 
 void setup(){
 
   Serial.begin(9600);
   tlc.begin();
   lcd.begin(16, 2);
+
+    for(int i = 0; i < 8; i++){
+    LEDValue[i][0] = 4000;
+    LEDValue[i][1] = 1000;
+    LEDValue[i][2] = 4000;
+    tlc.setLED(i, LEDValue[i][0], LEDValue[i][1] , 
+        LEDValue[i][2]);
+  }
 }
 
 
 void loop(){
 
-/*
-  if(millis() >= ledMillis)
-  {
-    ledMillis = 469 - (millis() - ledMillis) + millis();
-    blink();
-  }
-*/
   //displayText();
-  rainbow(1);
+  //rainbow(10);
+  quickFlash();
 
 }
 
-void displayText(){
+void test(){
+
+}
+void check(){
+
+    if(millis() >= ledMillis)
+    {
+      ledMillis = 469 - (millis() - ledMillis) + millis();
+      quickFlash();
+    }
+}
+// Fades LED brightness to 0, then back to normal again, similar to a music beat
+// Adjust variables named smooth, power1, and delayTime to adjust Flash
+void quickFlash(){
+	bpm++;
+	displayText(bpm);
+  doNotWrite = 1;
+  int smooth = 9; // 1/smooth, determines how many times led strength 
+          // will be divided in half before reaching 0
+  int savedMillis;
+  double power1 = 0.1;
+  double power = 1 - power1;
+  int delayTime = 2.7; // in milliseconds
+
+
+  for(int i = 1; i < smooth + 1; i++){
+    for(int j = 0; j < 8; j++){
+
+      tlc.setLED(j, LEDValue[j][0] * power, LEDValue[j][1] * power, 
+        LEDValue[j][2] * power);
+      delay(delayTime);
+      
+    }
+    power = power - power1;
+    tlc.write();
+    
+  }
+
+  // Then briefly go into darkness
+  //darkness();
+  delay(delayTime);
+
+  // Go back to full brightness of color
+
+  for(int i = smooth; i > 0; i--){
+    for(int j = 0; j < 8; j++){
+
+      tlc.setLED(j, LEDValue[j][0] * power, LEDValue[j][1] * power, 
+        LEDValue[j][2] * power);
+      delay(delayTime);
+      
+    }
+    power = power + power1;
+    tlc.write();
+    
+  }
+
+  doNotWrite = 0;
+
+
+}
+
+// Set all LED's to 0, or no light
+void darkness(){
+  for(int i = 0; i < 8; i++){
+    tlc.setLED(i, 0, 0, 0);
+  }
+  tlc.write();
+}
+
+
+void writeLED(int ledNum, int r, int g, int b){
+
+  LEDValue[ledNum][0] = r;
+  LEDValue[ledNum][1] = g;
+  LEDValue[ledNum][2] = b;
+  if(!doNotWrite){
+    tlc.setLED(ledNum, r, g, b);
+    tlc.write();
+  }
+
+}
+
+void displayText(double num){
   lcd.setCursor(0,1);
-  lcd.print("RAVE MODE");
-  lcd.setCursor(0,1);
-  lcd.print(" RAVE MODE");
+  lcd.print(num);
+  
 }
 
 // LED's in the sleeves undergo a rainbow color effect, displaying all colors
@@ -64,6 +153,7 @@ void rainbow(int interval){
       tlc.write();
       //delay(interval);
     }
+    check();
   }
   for(int i = 216; i > 8; i--){
     for(int j = 0; j < 8; j++){
@@ -72,6 +162,7 @@ void rainbow(int interval){
       tlc.write();
       //delay(interval);
     }
+    check();
   }
 
 
@@ -97,12 +188,12 @@ void blink(){
 
 void Wheel(uint8_t ledn, uint16_t WheelPos) {
   if(WheelPos < 1365) {
-    tlc.setLED(ledn, 3*WheelPos, 4095 - 3*WheelPos, 0);
+    writeLED(ledn, 3*WheelPos, 4095 - 3*WheelPos, 0);
   } else if(WheelPos < 2731) {
     WheelPos -= 1365;
-    tlc.setLED(ledn, 4095 - 3*WheelPos, 0, 3*WheelPos);
+    writeLED(ledn, 4095 - 3*WheelPos, 0, 3*WheelPos);
   } else {
     WheelPos -= 2731;
-    tlc.setLED(ledn, 0, 3*WheelPos, 4095 - 3*WheelPos);
+    writeLED(ledn, 0, 3*WheelPos, 4095 - 3*WheelPos);
   }
 }
